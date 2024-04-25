c545c9b4930eb1d4389512ee178e2d8f362f26a2
francobko <franco.condor@urp.edu.pe>
2024-04-25 16:29:33 -0500

    The most significant changes involve the addition of two new files, `RCurso.cs` and `SCurso.cs`, which introduce new classes for managing `CCurso` objects. The `RCurso.cs` file contains a repository class for storing `CCurso` objects, while the `SCurso.cs` file contains a service class that interacts with the repository. Additionally, the `MatriculaUPC.csproj` file has been modified to exclude the "Servicios" and "Repositorio" folders.
    1. The `MatriculaUPC.csproj` file was modified to remove the inclusion of the "Servicios" and "Repositorio" folders. This change suggests a restructuring of the project's organization.
    
    2. A new file `RCurso.cs` was added to the `MatriculaUPC.Repositorio` namespace. This file introduces a new internal class `RCurso` that serves as a repository for `CCurso` objects. The class includes a private list `listaCursos` for storing `CCurso` objects and methods `AgregarCurso` and `ObtenerCursos` for adding to and retrieving from the list, respectively.
    
    3. Another new file `SCurso.cs` was added to the `MatriculaUPC.Servicios` namespace. This file introduces a new internal class `SCurso` that interacts with the `RCurso` repository. The class includes a private `RCurso` object `repositorio` and methods `AgregarCurso` and `ObtenerCursos` for creating new `CCurso` objects and adding them to the repository, and for retrieving the list of `CCurso` objects from the repository, respectively.

---
+++ MatriculaUPC/Repositorio/RCurso.cs
@@ -0,0 +1,31 @@
+﻿using MatriculaUPC.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatriculaUPC.Repositorio
+{
+    internal class RCurso
+    {
+        // repository class
+
+        private List<CCurso> listaCursos;
+
+        public RCurso()
+        {
+            listaCursos = new List<CCurso>();
+        }
+
+        public void AgregarCurso(CCurso curso)
+        {
+            listaCursos.Add(curso);
+        }
+
+        public List<CCurso> ObtenerCursos()
+        {
+            return listaCursos;
+        }
+    }
+}
---
+++ MatriculaUPC/Servicios/SCurso.cs
@@ -0,0 +1,31 @@
+﻿using MatriculaUPC.Entidades;
+using MatriculaUPC.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatriculaUPC.Servicios
+{
+    internal class SCurso
+    {
+        private RCurso repositorio;
+
+        public SCurso()
+        {
+            repositorio = new RCurso();
+        }
+
+        public void AgregarCurso(String codigoCurso, String nombreCurso)
+        {
+            CCurso curso = new CCurso(codigoCurso, nombreCurso);
+            repositorio.AgregarCurso(curso);
+        }
+
+        public List<CCurso> ObtenerCursos()
+        {
+            return repositorio.ObtenerCursos();
+        }
+    }
+}
