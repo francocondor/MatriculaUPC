50c0a28da4d3ff9bb71e2eec43fd625eb0561536
francobko <franco.condor@urp.edu.pe>
2024-04-25 16:08:33 -0500

    Agregar archivos de proyecto.

---
+++ MatriculaUPC/Entidades/CCurso.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatriculaUPC.Entidades
+{
+    internal class CCurso
+    {
+        public String Codigo { get; set; }
+        public String Nombre { get; set; }
+    }
+}
