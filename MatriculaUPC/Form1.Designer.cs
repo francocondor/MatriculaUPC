e60a2ed0e08f7c3433ad93ebbcdc2d02943b19a4
francobko <franco.condor@urp.edu.pe>
2024-04-25 16:40:44 -0500

    The most important changes are in the `Form1.Designer.cs` and `Form1.cs` files. In `Form1.Designer.cs`, several new components were added to the form and configured with properties such as location, size, and name. The form's text was also changed. In `Form1.cs`, a new namespace was imported, a new private field was added and initialized, and two new methods were added.
    1. `Form1.Designer.cs`: The form's initial component setup lines were removed and several new components were added, including two labels, two text boxes, a button, and a data grid view. These new components were configured with properties such as location, size, and name. An event handler was added for the button's click event. The form's text was changed from "Form1" to "Matricula UPC".
    
    2. `Form1.cs`: A new namespace `MatriculaUPC.Servicios` was imported. A new private field `serviciosCurso` of type `SCurso` was added and initialized in the form's constructor. A method `ActualizarListaCursos` was called. Two new methods, `btnAgregarCurso_Click` and `ActualizarListaCursos`, were added. The `btnAgregarCurso_Click` method is currently empty. The `ActualizarListaCursos` method sets the data source of `dgListaCursos` to the result of `serviciosCurso.ObtenerCursos()`.
    
    3. `Form1.resx`: The changes were mostly formatting changes, with some comments being removed and others being added. The actual data in the file does not appear to have been changed.

--- MatriculaUPC/Form1.Designer.cs
+++ MatriculaUPC/Form1.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            txtCodigoCurso = new TextBox();
+            txtNombreCurso = new TextBox();
+            btnAgregarCurso = new Button();
+            dgListaCursos = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgListaCursos).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(80, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Codigo Curso";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(28, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(85, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Nombre Curso";
+            // 
+            // txtCodigoCurso
+            // 
+            txtCodigoCurso.Location = new Point(136, 22);
+            txtCodigoCurso.Name = "txtCodigoCurso";
+            txtCodigoCurso.Size = new Size(184, 23);
+            txtCodigoCurso.TabIndex = 2;
+            // 
+            // txtNombreCurso
+            // 
+            txtNombreCurso.Location = new Point(136, 63);
+            txtNombreCurso.Name = "txtNombreCurso";
+            txtNombreCurso.Size = new Size(184, 23);
+            txtNombreCurso.TabIndex = 3;
+            // 
+            // btnAgregarCurso
+            // 
+            btnAgregarCurso.Location = new Point(28, 108);
+            btnAgregarCurso.Name = "btnAgregarCurso";
+            btnAgregarCurso.Size = new Size(151, 23);
+            btnAgregarCurso.TabIndex = 4;
+            btnAgregarCurso.Text = "Agregar curso";
+            btnAgregarCurso.UseVisualStyleBackColor = true;
+            btnAgregarCurso.Click += btnAgregarCurso_Click;
+            // 
+            // dgListaCursos
+            // 
+            dgListaCursos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgListaCursos.Location = new Point(28, 162);
+            dgListaCursos.Name = "dgListaCursos";
+            dgListaCursos.Size = new Size(292, 219);
+            dgListaCursos.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dgListaCursos);
+            Controls.Add(btnAgregarCurso);
+            Controls.Add(txtNombreCurso);
+            Controls.Add(txtCodigoCurso);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Matricula UPC";
+            ((System.ComponentModel.ISupportInitialize)dgListaCursos).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtCodigoCurso;
+        private TextBox txtNombreCurso;
+        private Button btnAgregarCurso;
+        private DataGridView dgListaCursos;
     }
 }
--- MatriculaUPC/Form1.cs
+++ MatriculaUPC/Form1.cs
@@ -1,10 +1,29 @@
+using MatriculaUPC.Servicios;
+
 namespace MatriculaUPC
 {
     public partial class Form1 : Form
     {
+        private SCurso serviciosCurso;
+
         public Form1()
         {
             InitializeComponent();
+            serviciosCurso = new SCurso();
+            ActualizarListaCursos();
+        }
+
+        private void btnAgregarCurso_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ActualizarListaCursos()
+        {
+            dgListaCursos.DataSource = null;
+            var cursos = serviciosCurso.ObtenerCursos();
+            dgListaCursos.DataSource = cursos;
+
         }
     }
 }
